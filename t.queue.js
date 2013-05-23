6a04b8c682856cd4e0ad449cdda8a318970324e4
Lyu <lyu.career@gmail.com>
2013-05-23 22:07:39 +0800

    switched the type implementation from svn, including linkedlist, stack and queue

---
+++ t.queue.js
@@ -0,0 +1,39 @@
+(function(type, undefined){
+	type.Queue = function(){
+		this.__innerList__ = new T.LinkedList();
+		this.__emptyAccessError__ = new Error('access an empty queue');
+	};
+
+	type.Queue.prototype = {
+		isEmpty: function(){return this.__innerList__.isEmpty();},
+		
+		enqueue: function(item){
+			this.__innerList__.push(item);
+		},
+
+		peek: function(){
+			if (this.__innerList__.isEmpty()){
+				throw this.__emptyAccessError__;
+			}
+
+			return this.__innerList__.__header__.next.elem;
+		},
+
+		dequeue: function(){
+			var item = this.peek(),
+				_lst = this.__innerList__;
+			_lst.__removeNext__(_lst.__header__);
+			return item;
+		}, 
+
+		size: function(){
+			return this.__innerList__.size();
+		}, 
+
+		forEach: function(fn){this.__innerList__.forEach(fn);},
+
+		map: function(fn){return this.__innerList__.map(fn);},
+
+		toArray: function(){return this.__innerList__.toArray();}
+	};
+}(window.T = window.T || {}));
