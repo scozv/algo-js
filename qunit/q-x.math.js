d15c7bb8ab59f17d5ba1261d3d7f19e39a81fe68
Lyu <lyu.career@gmail.com>
2013-07-13 23:12:26 +0800

    reconstructed x.math.stats.js

--- qunit/q-x.math.js
+++ qunit/q-x.math.js
@@ -3,3 +3,19 @@ test('Math basic extensions', function(){
 	strictEqual(Math.mod(17, 4), 17 % 4, '17 mod 4 == 1');
 	strictEqual(Math.mod(-25, 4), 3, '-25 mod 4 == 3 (-25 == -7 * 4 + 3)');
 });
+
+test('Math.stats', function(){
+	var a1 = [1, 2, 3, 4, 5, 6, 7, 8];
+	var f = function (a, b){
+		return a1.map(function(x){return a*x + b;});
+	};
+
+	var f1 = function(a){
+		return a1.map(function(x){return Math.pow(x, a);});
+	};
+
+	ok(Math.Stats.linearLeastSquare(a1, f(3, 1)) - 3 < 1e-29, 'y=3x+1');
+	ok(Math.Stats.linearLeastSquare(a1, f(-2.31, 3.12)) - (-2.31) < 1e-29, 'y=-2.31x+3.12');
+	ok(Math.Stats.linearLeastSquare(a1, f1(2), Math.log) - (2) < 1e-29, 'y=x^2, with log');
+	ok(Math.Stats.linearLeastSquare(a1, f1(-3.141501), Math.log) - (-3.141501) < 1e-29, 'y=x^-3.141501, with log');
+});
--- x.math.stats.js
+++ x.math.stats.js
@@ -1,25 +1,46 @@
-(function(Stats, undefined){
-	// using x.array.js
-	
-	Stats.sum = function(arr){
-		return arr.reduce(function(acc, x){return x + acc;}, 0.0);
-	};
+(function(Math, undefined){
+	(function(Stats, undefined){
+		// using x.array.js
+		
+		Stats.sum = function(arr){
+			return arr.reduce(function(acc, x){return x + acc;}, 0.0);
+		};
 
-	Stats.mean = function(arr){
-		return Stats.sum(arr) / arr.length;
-	};
+		Stats.mean = function(arr){
+			return Stats.sum(arr) / arr.length;
+		};
 
-	// sample variance
-	Stats.var = function(arr){
-		var avg = Stats.mean(arr);
-		return arr.reduce(
-			function(acc, x){
-				return (x - avg) * (x - avg);
-			}, 
-			0.0) / (arr.length - 1);
-	};
+		// sample variance
+		Stats.var = function(arr){
+			var avg = Stats.mean(arr);
+			return arr.reduce(
+				function(acc, x){
+					return (x - avg) * (x - avg);
+				}, 
+				0.0) / (arr.length - 1);
+		};
 
-	Stats.stddev = function(arr){
-		return Math.sqrt(Stats.var(arr));
-	};
-}(window.Stats = window.Stats || {}));
+		Stats.stddev = function(arr){
+			return Math.sqrt(Stats.var(arr));
+		};
+
+		Stats.linearLeastSquare = function(arr1, arr2, fn){
+			// require(arr1.length == arr2.length)
+			if (fn && typeof fn === 'function'){
+				arr1 = arr1.map(fn);
+				arr2 = arr2.map(fn);
+			}
+
+			var avg1 = Stats.mean(arr1);
+			var avg2 = Stats.mean(arr2);
+
+			var m = 0, n = 0;
+			for (var i=0;i<arr1.length;i++){
+				m += (arr1[i]-avg1)*(arr2[i]-avg2);
+				n += (arr1[i]-avg1)*(arr1[i]-avg1);
+			}
+
+			return m / n;
+		};
+	}(Math.Stats = Math.Stats || {}));
+})(window.Math = window.Math || {});
