e9fa86a64643dd8dd1ac1960456359a4a10d5319
scott <scott@paradox-box>
2013-07-22 21:52:50 +0800

    added unit test for correctness of three sorting algorithms

--- qunit/q-sorting.js
+++ qunit/q-sorting.js
@@ -32,5 +32,22 @@ test('Sorting basic function', function(){
 	ok(uniqueArraySort(5), 'random array with unique element of length 5');
 	ok(uniqueArraySort(50), 'random array with unique element of length 50');
 	ok(uniqueArraySort(500), 'random array with unique element of length 500');
-	ok(uniqueArraySort(500000), 'random array with unique element of length 500000');
+	// ok(uniqueArraySort(500000), 'random array with unique element of length 500000');
+
+	var testIsSorted = function(length, fn, compare){
+		var arr = Sorting.__randomUniqueArray__(length);
+		arr.sort(function(x, y){return compare(fn(x), fn(y));});
+		return Sorting.isSorted(arr, fn, compare);
+	};
+
+	ok(testIsSorted(50, function(x){return x;}, function(x, y){return x-y;}), 'isSorte test for array of length 50, x=>x, x<y');
+	ok(testIsSorted(50, function(x){return x*x;}, function(x, y){return x-y;}), 'isSorte test for array of length 50, x=>x*x, x<y');
+	ok(testIsSorted(50, function(x){return x;}, function(x, y){return y-x;}), 'isSorte test for array of length 50, x=>x, x>y');
+});
+
+test('Correctness of sorting', function(){
+	var arr = Sorting.__randomUniqueArray__(10000);
+	ok(Sorting.isSorted(Sorting.mergeSort(arr)), 'merge sort for array of length 10000');
+	ok(Sorting.isSorted(Sorting.mergeSortBU(arr)), 'merge sort (bottom up version) for array of length 10000');
+	ok(Sorting.isSorted(Sorting.quickSort(arr)), 'quick sort for array of length 10000');
 });
