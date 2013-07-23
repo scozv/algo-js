184adce3429fe68c0038f08c1403f2b64ed1b8ba
Scott.L <scott.l@xg>
2013-07-23 17:41:52 +0800

    added range function for Math

--- qunit/q-x.math.js
+++ qunit/q-x.math.js
@@ -2,6 +2,18 @@ test('Math basic extensions', function(){
 	// strictEqual(Math.mod(0, 0), 0 % 0, '0 mod 0 == NaN');
 	strictEqual(Math.mod(17, 4), 17 % 4, '17 mod 4 == 1');
 	strictEqual(Math.mod(-25, 4), 3, '-25 mod 4 == 3 (-25 == -7 * 4 + 3)');
+	throws(function(){
+		Math.range();
+	}, 'range should have at least one args');
+
+	throws(function(){
+		Math.range('A', 1);
+	}, 'range accepts only numerical args');
+
+	deepEqual(Math.range(10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'Math.range(10)');
+	deepEqual(Math.range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9], 'Math.range(1, 10)');
+	deepEqual(Math.range(2, 11, 2), [2, 4, 6, 8, 10], 'Math.range(2, 11, 2)');
+	deepEqual(Math.range(2, 10, 2), [2, 4, 6, 8], 'Math.range(2, 10, 2)');
 });
 
 test('Math.stats', function(){
--- x.math.js
+++ x.math.js
@@ -9,6 +9,42 @@
 		return (i & 0x7FFFFFFF) % n;
 	};
 
+	Math.range = function(start, end, step){
+		// gets a range [start, end) with step
+		var arr = [],
+			i;
+
+		if (arguments.length == 0){
+			throw new Error('at least one argument.');
+		}
+
+		for (i=0;i<arguments.length;i++){
+			if ((+arguments[i]) !== arguments[i]){
+				throw new Error('all of arguments should be number.');
+			}
+		}
+
+		if (arguments.length == 1){
+			end = arguments[0];
+			start = 0;
+			step = 1;
+		} else if (arguments.length == 2){
+			start = arguments[0];
+			end = arguments[1];
+			step = 1;
+		} else {
+			start = arguments[0];
+			end = arguments[1];
+			step = arguments[2];
+		}
+
+		for (i=start;i<end;i+=step){
+			arr.push(i);
+		}
+
+		return arr;
+	};
+
 	Math.__timer__ = function(fn){
 		// stopwatch
 		var start = (new Date()).getTime();
