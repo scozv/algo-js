86875f520443bb088adbd7e9d8f0b796fa1a1ea8
scott@chnshad <scozv@yandex.com>
2015-05-19 16:58:39 +0800

    fix blanket coverage issue

--- src/t.js
+++ src/t.js
@@ -6,11 +6,8 @@
 		'INVALID_NUMERIC_VALUE'		: {writable: false, value: 'invalid numeric value has been passed'},
 		'INVALID_GRAPH_ACTION'		: {writable: false, value: 'invalid action on this graph, check it is directed or not'}
 	});
-
-	Object.defineProperty(type, 'ERROR',{
-		writable: false,
-		value: error
-	});
+	
+	type.ERROR = error;
 
 	var traversal = {};
 	Object.defineProperties(traversal, {
@@ -18,11 +15,8 @@
 		'IN_ORDER'	: {writable: false, value: 1},
 		'POST_ORDER': {writable: false, value: 2}
 	});
-
-	Object.defineProperty(type, 'TRAVERSAL',{
-		writable: false,
-		value: traversal
-	});
+	
+	type.TRAVERSAL = traversal;
 
 	type.__x__ = function (d, b) {
 		// inspired from http://www.typescriptlang.org/Playground/
